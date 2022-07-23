3608b7b34edd9c8fe4d2f4c74edef5a406da778b
Asidipta Chaudhuri <asidiptachaudhuri@gmail.com>
2022-07-23 11:03:41 +0530

    Updates api url

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000',
+  baseURL: 'https://social-app-backend-asi309.herokuapp.com',
 });
 
 export default api;
